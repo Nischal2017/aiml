72f036a00c030d4dd30b9ad28abf3dc3534ba1a6
Nischal-BK <32149646+Nischal2017@users.noreply.github.com>
2022-12-06 11:51:28 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,40 @@
+import numpy as np
+X = np.array(([2, 9], [1, 5], [3, 6]), dtype=float)
+y = np.array(([92], [86], [89]), dtype=float)
+X = X/np.amax(X,axis=0)
+y = y/100
+
+def sigmoid (x):
+    return 1/(1 + np.exp(-x))
+
+
+def dersig(x):
+    return x * (1 - x)
+
+
+e=7000
+lr=0.1 
+iln = 2 
+hln = 3
+oln = 1 
+
+wh=np.random.uniform(size=(iln,hln))
+bh=np.random.uniform(size=(1,hln))
+wout=np.random.uniform(size=(hln,oln))
+bout=np.random.uniform(size=(1,oln))
+
+for i in range(e):
+    h = np.dot(X,wh) + bh
+    hla = sigmoid(h)
+    oi= np.dot(hla,wout)+ bout
+    op = sigmoid(oi)
+    
+
+    dop = (y-op)* dersig(op)
+    EH = dop.dot(wout.T)
+    dhl = EH * dersig(hla)
+    wout += hla.T.dot(dop) *lr
+    wh += X.T.dot(dhl) *lr
+print("Input: \n" + str(X))
+print("Actual Output: \n" + str(y))
+print("Predicted Output: \n" ,op)
